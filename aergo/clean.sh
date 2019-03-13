b734e520ea3aead68fd77adb83f5e1e160237e3c
Jae-seok An <everjs@blocko.io>
2019-03-13 11:38:50 +0900

    clean.sh rm genesis*

--- aergo/clean.sh
+++ aergo/clean.sh
@@ -10,10 +10,8 @@ function rmall(){
 	rm -rf *.id *.key *.pub BP*.toml *.log
 }
 
-cd $ARG_TEST_SERVER
-
 killall -9 aergosvr
-rm -rf data*
+rm -rf data* genesis*
 
 # make empty args to string
 if [ "$1" = "all" ]; then
