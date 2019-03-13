ee1521baa1a8b6386441e0c93dc62842f8cfd2ce
Jae-seok An <everjs@blocko.io>
2019-03-13 11:36:02 +0900

    add new aergoconf-gen.sh

--- aergo/aergoconf-gen.sh
+++ aergo/aergoconf-gen.sh
@@ -1,72 +1,109 @@
-#!/bin/bash
+#!/bin/bash 
+# Usage: cmd <starting port> <template> <gen_start> <gen_end> <peer_start> <peer_end>"
+#
+# gen_start, gen_end: XXXX.toml 을 생성할 범위
+# peer_start, peer_end : peer로 들어갈 node의 범위
+
+# pkgen 명령 위치(path)를 적어줄 것
+pkgen="aergocli keygen"
+
+# pkey는 항상 0~22까지를 생성. 존재하면 생성 skip
 
 if [ $# != 3 ]; then
-    echo "Usage: $0 <starting port> <template> <max-from 0>"
+    echo "Usage: $0 <starting port> <template> <max>"
     exit
 fi
 
-# pkgen 명령 위치(path)를 적어줄 것
-#pkgen=/blockchain/BK134/pkgen
-
 port0=$1
-max=$3
 tmpl_file=$2
+max=$3
+gen_start=0
+gen_end=$(($max - 1))
 
-function gen_array()
-{
-    omit=$(($1 + 1))
-    shift
-    
-    j=0
-    for ((i=1; i <= $max; ++i))
-    do
-        if [ $i != $omit ]; then
-            out[j]=${!i}
-            j=$(($j + 1))
-        fi
-    done
+function gen_ids() {
+	for ((i=0; i < $max; i++))
+	do
+		out[i]=${id[$i]}
+	done
 
     echo ${out[*]} | sed -e 's= =\,=g' -e 's=\/=\\/=g'
 }
 
-for((i=0; i < $max; ++i))
-do
-    profileport=$(($port0 + $i + 100))
-    prof[i]=$profileport
-   
-    restport=$(($port0 + $i + 200))
-    rest[i]=$restport 
-    rpcport=$(($port0 + $i))
-    rpc[i]=$rpcport
+# gen_peers selfno startno endno ( 0 <= *no < $max)
+function gen_peers() {
+	self=$1
+	start=$2
+	end=$3
+
+	j=0
+	for ((i=$start; i <= $end; i++))
+	do
+		if [ $i != $self ]; then
+			out[j]=${peer[$i]}
+			j=$(($j + 1))
+		fi
+
+	done
+
+    echo ${out[*]} | sed -e 's= =\,=g' -e 's=\/=\\/=g'
+}
+
     
 
-    p2pport=$(($port0 + $i + 1000))
+# generate id[]
+for((i=0; i < $max; ++i))
+do
+    rpcport=$(($port0 + $i))
+    rpc[i]=$rpcport
+
+    profileport=$(($port0 + $i + 100))
+    prof[i]=$profileport
+
+    restport=$(($port0 + $i + 200))
+    rest[i]=$restport
+p2pport=$(($port0 + $i + 1000))
     p2p[i]=$p2pport
 
     pk[i]=${p2pport}.key
+    pk_id[i]=${p2pport}.id
 
     ofile[i]=BP${p2pport}.toml
 
-    aergocli keygen $p2pport
-    tempid=$(cat ${p2pport}.id)
-    id[i]="\"$(cat ${p2pport}.id)\""
-    peer[i]="\"/ip4/127.0.0.1/tcp/${p2pport}/p2p/${tempid}\""
+	# check if file exist
+	if [ ! -e ${pk[i]} ]; then
+		echo "${pk[i]}: $pkgen $p2pport"
+		$pkgen $p2pport
+	fi
 
+    tmpid=$(cat ${p2pport}.id)
+    id[i]="\"${tmpid}\""
+	peer[i]="\"/ip4/127.0.0.1/tcp/${p2pport}/p2p/${tmpid}\""
+	#echo "peer[ $i ]= ${peer[i]}"
 done
 
-ids=$(gen_array -1 ${id[*]})
+#ids=$(gen_ids ${id[*]})
+#echo $ids
 
-for((i=0; i < $max; ++i))
+#for((i=0; i < $max; ++i))
+for((i=$gen_start; i <= $gen_end; ++i))
 do
-    peers=$(gen_array ${i} ${peer[*]})
+    peers=$(gen_peers ${i} $gen_start $gen_end)
 
-    sed -e "s=_home_=${p2p[i]}=g" \
-        -e "s/_prof_/${prof[i]}/g" \
+#	echo  "${peers}"
+
+    echo "s=_home_=$PWD/${p2p[i]}=g" 
+    echo "s/_rpc_/${rpc[i]}/g" 
+    echo "s/_p2p_/${p2p[i]}/g"
+    echo "s/_peer_/${peers}/g" 
+    echo "s/_pk_/${pk[i]}/g" 
+
+    sed -e "s=_home_=$PWD/${p2p[i]}=g" \
+        -e "s=_data_=$PWD/data${p2p[i]}=g" \
         -e "s/_rest_/${rest[i]}/g" \
+	-e "s/_prof_/${prof[i]}/g" \
 	-e "s/_rpc_/${rpc[i]}/g" \
         -e "s/_p2p_/${p2p[i]}/g" \
         -e "s/_peer_/${peers}/g" \
-        -e "s/_bps_/$max/g" \
-	-e "s/_pk_/${pk[i]}/g" \
-        -e "s/_ids_/${ids}/g" $tmpl_file >${ofile[i]}
+        -e "s/_pk_/${pk[i]}/g"  $tmpl_file >${ofile[i]}
 done
+echo $(pwd)
